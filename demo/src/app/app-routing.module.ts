dab1411b99d65fa952e0b560c1d86463a605cf0c
Paris Hilario <parishilario500@gmail.com>
2022-07-30 16:43:22 +0800

    Connected login component to student dashboard.

--- demo/src/app/app-routing.module.ts
+++ demo/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { SchoolComponent } from './registration/school/school.component';
 import { StudentComponent } from './registration/student/student.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
 
 const routes: Routes = [
   {path: 'login',  title: 'Edu-Aksyon | Login', component: LoginComponent},
   {path: 'registration',  title: 'Edu-Aksyon | Registration', component: RegistrationComponent},
   {path: 'schoolRegistration',  title: 'Edu-Aksyon | School Registration', component: SchoolComponent},
   {path: 'studentRegistration',  title: 'Edu-Aksyon | School Registration', component: StudentComponent},
+  {path: 'studentDashboard',  title: 'Edu-Aksyon | School Registration', component: StudentDashboardComponent},
 ];
 
 @NgModule({
